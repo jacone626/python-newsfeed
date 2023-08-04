e137894edbf236ffca0a2d23986c3c062095c7f8
James A Cone <jacone626@gmail.com>
2023-08-04 10:51:33 -0400

    added init_db function and got posts to display on homepage

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from app.routes import home, dashboard
 from flask import Flask
+from app.db import init_db
 
 def create_app(test_config=None):
   # set up app config
@@ -14,4 +15,7 @@ def create_app(test_config=None):
  
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
+
+  init_db()
+
   return app
